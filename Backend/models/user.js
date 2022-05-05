d62fccffd56c879e6257f527ef3b35ea7d0e9c08
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-05-05 18:25:04 +0430

    user model created

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -4,5 +4,17 @@ const sequelize=require('../util/database');
 
 
 const user=sequelize.define('user',{
-    
+    id:{
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true,
+    },name:{
+        type:Sequelize.STRING,
+        allowNull:false,
+    },
+    family:{type:Sequelize.STRING,allowNull:false},
+    email:{type:Sequelize.STRING,allowNull:false,unique:true},
+    phone:{type:Sequelize.STRING,allowNull:false,unique:true}
 })
+module.exports=user;
