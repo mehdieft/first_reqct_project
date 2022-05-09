cd7693a7a6f63da557e327b95d9d5d561fbfdde7
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-05-09 14:42:11 +0430

    stop working  on frontend

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,13 +1,20 @@
 import { FiTrash } from "react-icons/fi";
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import {BsPencilFill}  from "react-icons/bs";
+import {useState} from 'react';
 const Todo=({item,onDeleteTodo})=>{
    
+    const [title,titleState]=useState(item.title);
+    const [content,contentState]=useState(item.content)
     return (
         <div className="my-2" style={{padding:'20px',border:'1px solid black',borderRadius:'4PX'}}>
             <h2  style={{backgroundImage:'linear-gradient(to right, rgba(255,0,0,0), rgb(0 255 153))',color:'black'}} className=" text-center bg-red">
-             {item.title}
+             {title}
                 <button onClick={()=>onDeleteTodo(item.id)} style={{float:'right'}} className="btn float-right"><FiTrash/></button>
+                <button className="btn btn-danger" style={{float:'right'}} ><BsPencilFill/></button>
             </h2>
-            <p className="p-3">{item.content}</p>
+            <p className="p-3 text-white">{content}</p>
             
 
         </div>
--- frontend/src/components/todofield.js
+++ frontend/src/components/todofield.js
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 const TodoField = () => {
+    const [userInput,onUserInput]=useState({
+        title:'',
+        content:''
+    })
+    // const [title, SetTitle] = useState('');
+    // const [content, setContent] = useState('');
+    const titleChange = (event) => { 
+        onUserInput({...userInput,
+        title:event.target.value
+        })
+
+    console.log("title change",event.target.value);
+    }
+    const ContentChange = (event) => { onUserInput({...userInput,content:event.target.value});
+    console.log("content change==>",event.target.value);
+    }
     return (
-        <div className="d-flex flex-column w-80 mh-100 overflow-auto">
+        <div className="d-flex flex-column w-80 mh-100 ">
             <h2>
                 todo field type
             </h2>
-            <input type="text" className="mt-4 mb-4 form-control form-control-sm" />
+            <input type="text" className="mt-4 mb-4 form-control form-control-sm" onChange={titleChange} />
             <div className="input-group">
                 <div className="input-group-prepend">
-                    
+
                 </div>
-                <textarea class="form-control my-4" style={{height:'150px' ,overflow:'auto'}} aria-label="With textarea"></textarea>
+                <textarea class="form-control my-4" onChange={ContentChange} style={{ height: '60px', overflow: 'auto' }} aria-label="With textarea"></textarea>
             </div>
 
             <button className="btn btn-success" type="submit">
