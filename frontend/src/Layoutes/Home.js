7b65deb18be528727bdefd2180585649069d18de
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-05-10 19:03:04 +0430

    having bug right now

--- frontend/src/Layoutes/Home.js
+++ frontend/src/Layoutes/Home.js
@@ -18,17 +18,32 @@ const Home=()=>{
     ])
 
     const deleteTask=(id)=>{
-        setTodo(todo.filter((item)=>item.id != id))
+        setTodo(todo.filter((item)=>item.id !== id))
+        
+    }
+
+
+    const saveData=(todoObject)=>{
+        console.log("this is object parrent",todoObject)
+        const newTodo={
+            ...todoObject,
+            id:Math.random()*100
+        }
+        console.log("this is new item",newTodo)
+        setTodo((todo)=>{
+            todo.push(newTodo)
+        });
+
     }
     return (
         <div style={{backgroundColor:'red'}}>
 
-        <div className="row container  m-auto" style={{backgroundColor:'red'}}>
+        <div className="row container  m-auto" style={{backgroundColor:'#7b6868',marginTop:'40px'}}>
        
         <div className=" text-center">
 
-            <div style={{display:'inline-block',width:'50vh',height:'50vh'}}>
-                <TodoField />
+            <div style={{display:'inline-block',width:'50vh',height:'50vh',marginTop:'20px'}}>
+                <TodoField onsaveTodofield={saveData} />
 
             </div>
             <div style={{display:'inline-block', width:'70vh',marginLeft:'40px',float:'right'}}>
