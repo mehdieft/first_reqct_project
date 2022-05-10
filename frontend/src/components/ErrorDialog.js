433acf34faa94ca3584608bf7a7a704b8fd23b39
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-05-10 20:08:09 +0430

    error dialog complete

--- frontend/src/components/ErrorDialog.js
+++ frontend/src/components/ErrorDialog.js
@@ -5,11 +5,19 @@ const ErrorDialog=({title,content})=>{
         <>
         <div className="error-wrapper">
             <div className="row h-80">
-            <h1 className="col-8 col-md-9 col-sm-8 text-center mx-auto">title</h1>
+            <h1 className="col-8 col-md-9 col-sm-8 text-center mx-auto">{title}</h1>
             <MdCancel   className="col-1 col-sm-1 col-md-1 m-0" />
+            </div>
+            <hr></hr>
+            <div>
+                <p>{content}</p>
             </div>
         </div>
         </>
     )
+}
+ErrorDialog.defaultProps = {
+    title:'error',
+    content:'something wrong happenned please check '
 }
 export default ErrorDialog
